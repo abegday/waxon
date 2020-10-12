4f5b09359ee625b636c5ceb9537c3a7851a124a2
abegday <abegday@gmail.com>
2020-10-12 21:27:56 +0300

    added responsive

--- js/maine.js
+++ js/maine.js
@@ -1,30 +1,50 @@
-(function ($){
-    $(document).ready(function (){
+(function ($) {
+    $(document).ready(function () {
         $('.banner-slider').slick({
-            prevArrow:"<div class='arrow-prev'></div>",
-            nextArrow:"<div class='arrow-next'></div>",
-            dots:true,
-             customPaging : function(slider, i) {
-             return ''
+            prevArrow: "<div class='arrow-prev'></div>",
+            nextArrow: "<div class='arrow-next'></div>",
+            dots: true,
+            customPaging: function (slider, i) {
+                return ''
             },
+            responsive: [{
+                breakpoint: 768,
+                settings: {
+                    arrows: false
+                }
+            }]
         });
 
-       
-
-        $('.recent-post-img-slider').slick({
-            arrows:false,
-            dots:true,
-             customPaging : function(slider, i) {
-             return ''
-            },
-        });
 
         $('.recent-post-big-slider').slick({
+            slidesToShow: 3,
             prevArrow: $(".recent-post-big-arrows .arrow-prev"),
             nextArrow: $(".recent-post-big-arrows .arrow-next"),
-            dots:false,
+            dots: false,
+            responsive: [{
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+
+            ]
         });
+
+
+        $('.header-burger').click(function () {
+            $('.nav').toggleClass('active')
+        })
+
+
+
 
     })
 })(jQuery)
-
